b0afaa3d3cb3ca1d48ad021708ba2130724571e3
SathishSaminathan <Sathishcse1975@gmail.com>
2018-06-24 16:37:48 +0530

    Added Font in Get-Started-Page

---
+++ src/assets/styles/Common.js
@@ -0,0 +1,23 @@
+export default { 
+    // COLOR_PRIMARY : '#ff1e8ff5',// dark-pink
+    COLOR_PRIMARY : '#ff265bba',//light-pink
+    COLOR_PRIMARY_BLUE : '#008388',//blue
+    COLOR_SECONDARY : '#58C9B9',
+    COLOR_PRIMARY_PINK : "#fc6c8f",
+    COLOR_PRIMARY_PINK_BACKGROUND : "#b11339c2",
+    PRIMARY_YELLOW_COLOR:"#ffff11",
+    PINK_TRANSPARENT: "#ea87b96b",
+    BUTTON_PINK:"#ff1e8ff5",
+    SUCCESS_COLOR :"#5cb85c",
+    DANGER_COLOR : "#d9534f",
+    PRIMARY_COLOR : "#2c3e50",
+    SITE_GRAY1 : "#54575e",
+    FONT_NORMAL : 'OpenSans-Regular',
+    FONT_BOLD : 'OpenSans-Bold',
+    BORDER_RADIUS : 5,
+    XXLARGE : 60,
+    XLARGE : 40,
+    LARGE : 30,
+    MEDIUM : 24,
+    SMALL : 15,
+}
--- src/screens/GetStarted.js
+++ src/screens/GetStarted.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import { View, Text, StyleSheet,Dimensions,Image,TouchableOpacity,StatusBar } from 'react-native';
 import * as Animatable from "react-native-animatable";
 
+import colorFonts from "../assets/styles/Common";
 import images from "../assets/img/images";
 
 
@@ -19,7 +20,7 @@ class GetStarted extends Component {
 
                 <StatusBar 
                     barStyle="dark-content"
-                    backgroundColor="#ffff11"
+                    backgroundColor={colorFonts.PRIMARY_YELLOW_COLOR}
                     translucent={false}
                 />
                 {/* TOP */}
@@ -27,12 +28,30 @@ class GetStarted extends Component {
                     animation="slideInDown"
                     iterationCount={1}
                     duration={200}
-                    style={{height:BACKGROUND_IMAGE_HEIGHT,width:width,zIndex:0}}              
+                    style={{
+                        height:BACKGROUND_IMAGE_HEIGHT,
+                        width:width,
+                        zIndex:0
+                    }}              
                 >
                     <Image 
                         source={images.appBackground} 
                         style={{flex:1,height:undefined,width:undefined}}
                     /> 
+                    <Animatable.View
+                        animation="zoomIn"
+                        duration={300}
+                        style={{
+                            position:"absolute",
+                            marginTop:BACKGROUND_IMAGE_HEIGHT/3,
+                            height:BACKGROUND_IMAGE_HEIGHT/4,
+                            width:width/1.5,
+                            backgroundColor:"white",
+                            elevation:5,
+                            alignSelf:"center"
+                        }}
+                    >
+                    </Animatable.View>
                 </Animatable.View >
                 {/* TOP */}
 
@@ -46,35 +65,46 @@ class GetStarted extends Component {
                     <View
                         style={{
                             flex:1,
-                            flexDirection:"row",
+                            flexDirection:"column",
                             justifyContent:"space-around",
                             alignContent:"center",
                             backgroundColor:"black"
                         }}
                     >
-                        <Text
+                        <View
                             style={{
-                                color:"#ffff11",
-                                fontSize:28
+                                flexDirection:"row",
+                                justifyContent:"space-between",
+                                paddingHorizontal:20
                             }}
                         >
-                            Get
-                        </Text>                        
-                        <Text                        
-                            style={{
-                                color:"#ffff11",
-                                fontSize:28
-                            }}
-                        >
-                            Started
-                        </Text>
+                            <Text
+                                style={{
+                                    color:colorFonts.PRIMARY_YELLOW_COLOR,
+                                    fontSize:colorFonts.LARGE,
+                                    fontFamily:"Roboto-Light"
+                                }}
+                            >
+                                Get
+                            </Text>                        
+                            <Text                        
+                                style={{
+                                    color:colorFonts.PRIMARY_YELLOW_COLOR,
+                                    fontSize:colorFonts.LARGE,
+                                    fontFamily:"Roboto-Light"
+                                }}
+                            >
+                                Started
+                            </Text>
+                        </View>
                     </View>
                 </Animatable.View >         
                 {/* Bottom */}
                 
                 <Animatable.View  
-                    animation="bounceIn"
-                    iterationCount={1}               
+                    animation="bounceIn"        
+                    iterationCount={1}    
+                    duration={1500}           
                     style={{zIndex:1,alignSelf:"center",height:APP_ICON_HEIGHT,width:APP_ICON_HEIGHT,position:"absolute",borderRadius:APP_ICON_HEIGHT,marginTop:BACKGROUND_IMAGE_HEIGHT-(APP_ICON_HEIGHT/2)}}
                 >  
                     <TouchableOpacity
