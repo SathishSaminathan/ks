5b601dfc236f4c566fcb82d9083ba1da5c2593a3
SathishSaminathan <Sathishcse1975@gmail.com>
2018-06-24 15:41:07 +0530

    Adding Image in Get-Started-Page

--- src/screens/GetStarted.js
+++ src/screens/GetStarted.js
@@ -1,28 +1,91 @@
 //import liraries
 import React, { Component } from 'react';
-import { View, Text, StyleSheet,Dimensions,Image } from 'react-native';
+import { View, Text, StyleSheet,Dimensions,Image,TouchableOpacity,StatusBar } from 'react-native';
+import * as Animatable from "react-native-animatable";
+
 import images from "../assets/img/images";
 
 
 const {width,height} = Dimensions.get("window");
+
+BACKGROUND_IMAGE_HEIGHT = height/1.2
+APP_ICON_HEIGHT=150
 
 // create a component
 class GetStarted extends Component {
     render() {
         return (
             <View style={styles.container}>
-                <View
-                    // style={{height:height/1.5, backgroundColor:"red"}}
+
+                <StatusBar 
+                    barStyle="dark-content"
+                    backgroundColor="#ffff11"
+                    translucent={false}
+                />
+                {/* TOP */}
+                <Animatable.View  
+                    animation="slideInDown"
+                    iterationCount={1}
+                    duration={200}
+                    style={{height:BACKGROUND_IMAGE_HEIGHT,width:width,zIndex:0}}              
                 >
                     <Image 
                         source={images.appBackground} 
-                        style={{height:height/1.5,width:width}}
-                    />
-                </View>
-                <View
-                    style={{flex:1, backgroundColor:"green"}}
+                        style={{flex:1,height:undefined,width:undefined}}
+                    /> 
+                </Animatable.View >
+                {/* TOP */}
+
+                {/* Bottom */}
+                <Animatable.View 
+                    animation="slideInUp"
+                    iterationCount={1}
+                    duration={200}
+                    style={{flex:1}}
                 >
-                </View>
+                    <View
+                        style={{
+                            flex:1,
+                            flexDirection:"row",
+                            justifyContent:"space-around",
+                            alignContent:"center",
+                            backgroundColor:"black"
+                        }}
+                    >
+                        <Text
+                            style={{
+                                color:"#ffff11",
+                                fontSize:28
+                            }}
+                        >
+                            Get
+                        </Text>                        
+                        <Text                        
+                            style={{
+                                color:"#ffff11",
+                                fontSize:28
+                            }}
+                        >
+                            Started
+                        </Text>
+                    </View>
+                </Animatable.View >         
+                {/* Bottom */}
+                
+                <Animatable.View  
+                    animation="bounceIn"
+                    iterationCount={1}               
+                    style={{zIndex:1,alignSelf:"center",height:APP_ICON_HEIGHT,width:APP_ICON_HEIGHT,position:"absolute",borderRadius:APP_ICON_HEIGHT,marginTop:BACKGROUND_IMAGE_HEIGHT-(APP_ICON_HEIGHT/2)}}
+                >  
+                    <TouchableOpacity
+                        style={{flex:1}}
+                    >
+                    <Image 
+                        source={images.appLogo} 
+                        style={{flex:1,height:undefined,width:undefined}}
+                    />  
+                    </TouchableOpacity>
+                </Animatable.View>  
             </View>
         );
     }
